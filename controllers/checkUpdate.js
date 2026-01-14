dc753229c13c42788658d053132799ec2ac97ec7
Saurabh Shelar <saurabhshelar512@gmail.com>
2026-01-14 15:16:29 +0530

    version changed tooo  11 and added logs to test

--- controllers/checkUpdate.js
+++ controllers/checkUpdate.js
@@ -3,23 +3,29 @@ const APK_URL = "https://notes-taking-app-283p.onrender.com/apk/locApp-release.a
 
 const checkUpdate = (req, res) => {
   // const { versionCode } = req.body;
+  console.log("=== CHECK UPDATE HIT ===");
+  console.log("Raw body:", req.body);
   const versionCode = Number(req.body.versionCode);
 
-
+  console.log("Parsed versionCode:", versionCode);
+  console.log("Latest version:", LATEST_VERSION);
   if (typeof versionCode !== "number") {
+      console.log("❌ versionCode is NaN");
     return res.status(400).json({
       error: "versionCode is required and must be a number"
     });
   }
 
   if (versionCode < LATEST_VERSION) {
+     console.log("✅ UPDATE AVAILABLE");
     return res.status(200).json({
       updateAvailable: true,
       latestVersionCode: LATEST_VERSION,
       apkUrl: APK_URL,
-      forceUpdate: true 
+      forceUpdate: true
     });
   }
+    console.log("ℹ️ App is up to date");
 
   return res.status(200).json({
     updateAvailable: false,
