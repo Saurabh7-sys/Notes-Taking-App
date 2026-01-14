62ec32a79f5938f320643d4b74a27c7e1625b89a
Saurabh Shelar <saurabhshelar512@gmail.com>
2026-01-14 12:45:38 +0530

    version changed

--- controllers/checkUpdate.js
+++ controllers/checkUpdate.js
@@ -1,4 +1,4 @@
-const LATEST_VERSION = 3;
+const LATEST_VERSION = 5;
 const APK_URL = "https://notes-taking-app-283p.onrender.com/apk/locApp-release.apk";
 
 const checkUpdate = (req, res) => {
@@ -26,3 +26,6 @@ const checkUpdate = (req, res) => {
 };
 
 module.exports = { checkUpdate };
+
+
+
